07e0b2f51ef48dbfd22bfa19f4ade15d9e79f986
Pierre Gilot <pierre.gilot@me.com>
2015-09-28 21:20:41 +0200

    preparing environment

--- ticTacSprite/GameScene.swift
+++ ticTacSprite/GameScene.swift
@@ -9,10 +9,21 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let screenSize: CGRect = UIScreen.mainScreen().bounds
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        // Setup the board
+        let boardNode = SKSpriteNode(imageNamed: "board")
+        boardNode.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        let size = min(screenSize.width, screenSize.height)
+        boardNode.size = CGSize(width:size, height:size)
+        self.addChild(boardNode)
+        
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
+        myLabel.text = "Tic Tac Toe";
         myLabel.fontSize = 45;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
@@ -24,11 +35,14 @@ class GameScene: SKScene {
         
         for touch in touches {
             let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            print(touchedNode)
+            let sprite = SKSpriteNode(imageNamed:"x")
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
+            //sprite.xScale = 0.5
+            //sprite.yScale = 0.5
+            let mySize = min(screenSize.width, screenSize.height) / 3
+            sprite.size = CGSize(width:mySize, height:mySize)
             sprite.position = location
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
